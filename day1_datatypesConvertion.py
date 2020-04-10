b36515c49b78a725485f7a41cbf2aff9816c90d7
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-10 22:53:42 +0700

    basic datatype conversion yeaaay 22:53

---
+++ day1_datatypesConvertion.py
@@ -0,0 +1,8 @@
+
+x = input("Yaboii insert some number here : ")
+print(str(x)+ " is your number.")
+
+print("\nwe do additon to your number by 10  \n")
+print(int(x) + 10)
+
+#for float we use float(var)
