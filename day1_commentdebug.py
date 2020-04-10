eee203cc44ab8dedd4a0f909ee0e023f2d2f7e45
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-10 22:02:43 +0700

    22:00

---
+++ day1_commentdebug.py
@@ -0,0 +1,7 @@
+print("addition")
+# this wont be exectuted
+y = int(input("please enter y value : "))
+x = y + 300
+print("yaboii its dividing time")
+x = x / 0
+print("YEA ITS FINISHED")
---
+++ day1_stringstuff.py
@@ -0,0 +1,5 @@
+#first = "fahri"
+#last = "novald"
+first = input("Please input your fucking name : ")
+last = input("and your last name too, lol : ")
+print("Hello, " + first.capitalize + " " + last.capitalize)
