6e94c6ff35eb564e1f9f52422c6c7fa530e52d30
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-10 22:35:09 +0700

    string formatting yeaay 22:35

--- day1_stringstuff.py
+++ day1_stringstuff.py
@@ -2,4 +2,15 @@
 #last = "novald"
 first = input("Please input your fucking name : ")
 last = input("and your last name too, lol : ")
-print("Hello, " + first.capitalize + " " + last.capitalize)
+print("Hello, " + first.capitalize() + " " + last.capitalize())
+
+
+# formatting string stuff
+
+output = "Hello, {1} {0}".format(first, last)
+
+print(output)
+
+output = f"hello, {first.capitalize()} {last.capitalize()}"
+
+print(output)
