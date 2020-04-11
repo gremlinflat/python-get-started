0026d55a450053ebd8243d0f9c2af2bec3764c9f
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-12 00:34:26 +0700

    timedelta fuction, its about how to count day

--- day2_datetime.py
+++ day2_datetime.py
@@ -4,8 +4,10 @@ current_date = datetime.now()
 
 tanggal = input("Please input your first date : ")
 first_date = datetime.strptime(tanggal, "%d/%m/%Y" ) 
+yesterday = timedelta(days=1)
 
 print(f"your first date is on {first_date}")
+print(f"the day before it was {first_date-yesterday}")
 print(f"day {first_date.day}")
 print(f"month {first_date.month}")
 print(f"year {first_date.year}")
