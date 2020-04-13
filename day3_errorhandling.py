113dd24e9a67b3187e89c7c4229b4bf277e15836
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-13 14:24:44 +0700

    hufft just finished error handling

--- day3_errorhandling.py
+++ day3_errorhandling.py
@@ -1,4 +1,11 @@
+x = 100
+y = int(input("Please enter the number to dived 100 : "))
+
 try:
-    pass
-except expression as identifier:
-    pass
+    print(x/y)
+except ZeroDivisionError as o:
+    print("Youre not allowed to divide number with zero")
+else:
+    print("Something else happening except zero")
+finally:
+    print("Yeay this handling stuff is finished")
