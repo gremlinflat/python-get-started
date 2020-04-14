8d9bd2a2b2ab011dc2ada44a0f899fcaa56def89
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-15 00:58:48 +0700

    including array yeay 0:58 day 5

--- day4_collectionStuff.py
+++ day4_collectionStuff.py
@@ -12,3 +12,11 @@ people.append(SUSAN)
 people.append({"first": "joko", "last": "widoddo"})
 print(people)
 print(people[0]["first"])
+
+from array import array
+
+kamus = array('f')
+kamus.append(88)
+kamus.append(34124)
+
+print(kamus[1])
