7d2759da76141f6efbb266c7f18d8755e732cb8d
Fahri Novaldi <fahri.119140205@student.itera.ac.id>
2020-04-16 16:27:46 +0700

    just accuire 3 looping technique yeeeay

---
+++ day5_loops.py
@@ -0,0 +1,22 @@
+
+name = []
+cunt = int(input("How many name do you want to input? "))
+
+#inputing arrray
+for b in range(cunt):
+    name.append(input())
+
+print()
+for c in name:
+    print(c)
+
+#printing number in range
+
+for d in range(cunt):
+    print(d)
+
+#using while 
+index = 0
+while index < len(name):
+    print(name[index])
+    index = index + 1
